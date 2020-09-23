0d4f87f1d924513d051d6293c51d9f7f9c473f68
JayaNaveenSai <naveen.sai@nineleaps.com>
2020-09-23 15:37:38 +0530

    angular admin dashboard

---
+++ src/app/shared/widgets/card/card.component.ts
@@ -0,0 +1,91 @@
+import { Component, OnInit, Input } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
+
+@Component({
+  selector: 'app-widget-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+@Input() label:string;
+@Input() total:string;
+@Input() percentage:string;
+
+
+  Highcharts= Highcharts;
+  chartOptions={}
+  constructor() { }
+
+@Input( ) data=[];
+
+  ngOnInit() {
+    this.chartOptions = {
+      chart: {
+        type: 'area',
+        backgroundColor: null,
+        borderWidth: 0,
+        margin: [2, 2, 2, 2],
+        height: 60
+
+        
+      },
+      title: {
+        text: null
+      },
+      subtitle: {
+        text: null
+      },
+      credits: {
+        enabled: false
+      },
+      exporting:{
+        enabled:false
+      },
+      tooltip: {
+        split: true,
+        outside:true
+      
+      },
+      legend:{
+       enabled:false
+      },
+
+      series: [{
+        data:this.data
+        
+      }],
+      xAxis: {
+        labels: {
+          enabled: false,
+        },
+        title: {
+          text: null
+        },
+        startOnTick: false,
+        endOnTick: false,
+        tickOptions: []
+      },
+      yAxis: {
+        labels: {
+          enabled: false,
+        },
+        title: {
+          text: null
+        },
+        startOnTick: false,
+        endOnTick: false,
+        tickOptions: []
+      },
+    };
+
+    HC_exporting(Highcharts);
+
+      setTimeout(() => {
+        window.dispatchEvent(
+          new Event('resize')
+        );
+      }, 300);
+  }
+
+}
